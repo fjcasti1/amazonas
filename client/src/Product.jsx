023944238626b7d223f18c8b3ded9801a61f4958
Francisco Castillo <fjcasti1@asu.edu>
2020-12-19 02:02:45 -0700

    Create Product & Rating Components

---
+++ client/src/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Rating from './components/Rating';
+
+const Product = ({ product }) => {
+  const { _id, name, image, price, rating, numReviews } = product;
+  return (
+    <div key={_id} className='card'>
+      <a href={`/product/${_id}`}>
+        <img className='medium' src={image} alt='product' />
+      </a>
+      <div className='card-body'>
+        <a href={`/product/${_id}`}>
+          <h2>{name}</h2>
+        </a>
+        <Rating rating={rating} numReviews={numReviews} />
+        <div className='price'>${price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
