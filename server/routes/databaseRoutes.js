ba7c62224a9f9787b93a535a8336f2c2217b09ea
Francisco Castillo <fjcasti1@asu.edu>
2020-12-31 19:54:26 -0700

    Add product Routes

--- server/routes/databaseRoutes.js
+++ server/routes/databaseRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 import data from '../data.js';
 import User from '../models/userModel.js';
 import expressAsyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
 
 const databaseRouter = express.Router();
 
-// @route     POST api/database/users/seed
+// @route     GET api/database/users/seed
 // @desc      Seed users data to database
 // @access    Public
 databaseRouter.get(
@@ -16,7 +17,7 @@ databaseRouter.get(
   }),
 );
 
-// @route     POST api/database/users/clear
+// @route     GET api/database/users/clear
 // @desc      Clear users data from database
 // @access    Public
 databaseRouter.get(
@@ -24,6 +25,28 @@ databaseRouter.get(
   expressAsyncHandler(async (req, res) => {
     await User.deleteMany({});
     res.send({ msg: 'users removed' });
+  }),
+);
+
+// @route     GET api/database/products/seed
+// @desc      Seed products data to database
+// @access    Public
+databaseRouter.get(
+  '/products/seed',
+  expressAsyncHandler(async (req, res) => {
+    const createProducts = await Product.insertMany(data.products);
+    res.json(createProducts);
+  }),
+);
+
+// @route     GET api/database/products/clear
+// @desc      Clear products data from database
+// @access    Public
+databaseRouter.get(
+  '/products/clear',
+  expressAsyncHandler(async (req, res) => {
+    await Product.deleteMany({});
+    res.send({ msg: 'products removed' });
   }),
 );
 
--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ import express from 'express';
 import 'colors';
 import dotenv from 'dotenv';
 import connectDB from './config/mongoDB.js';
-
-import data from './data.js';
 import databaseRouter from './routes/databaseRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 const app = express();
 
@@ -18,19 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 // Routes
 app.use('/api/database', databaseRouter);
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => res.send('API running...'));
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not Found' });
-  }
-});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
