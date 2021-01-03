eee095e3330b5f9d2237b1639bee3f22cc3acaf8
Francisco Castillo <fjcasti1@asu.edu>
2021-01-03 01:13:24 -0700

    Add pay order endpoint

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -62,4 +62,34 @@ orderRouter.get(
   }),
 );
 
+// @route     PUT api/orders/:id/pay
+// @desc      Pay order by Id
+// @access    Private
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const { transactionId, status, update_time, email_address } = req.body; // From paymentResult
+
+    const orderId = req.params.id;
+
+    const order = await Order.findById(orderId);
+
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: transactionId,
+        status,
+        update_time,
+        email_address,
+      };
+      const updatedOrder = order.save();
+      res.send({ message: 'Order Paid', order: updatedOrder });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  }),
+);
+
 export default orderRouter;
