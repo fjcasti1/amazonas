ddcf08f67516d6232e09e406ec0a0d59554c9329
Francisco Castillo <fjcasti1@asu.edu>
2021-01-02 17:48:07 -0700

    Add order routes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongoDB.js';
 import databaseRouter from './routes/databaseRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/database', databaseRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.get('/', (req, res) => res.send('API running...'));
 
