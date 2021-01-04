55e5705fb46c87f80914dd4b22b734392da80eec
Francisco Castillo <fjcasti1@asu.edu>
2021-01-04 01:37:13 -0700

    Put react-redux imports in one line

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { logout } from '../actions/userActions';
 
 const Header = () => {
