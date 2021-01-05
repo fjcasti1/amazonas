7d8d7b84ee42e51507d8f87f8caf2d2a0f2193ed
Francisco Castillo <fjcasti1@asu.edu>
2021-01-04 19:30:57 -0700

    Add delete route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -117,4 +117,22 @@ productRouter.post(
   },
 );
 
+// @route     DELETE api/products/:id
+// @desc      Delete product by Id
+// @access    Private Admin
+productRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      await product.remove();
+      res.send({ message: 'Product deleted' });
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
+  }),
+);
+
 export default productRouter;
