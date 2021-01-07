76630d3481339f948eca878cb57b9a98ffafcbd2
Francisco Castillo <fjcasti1@asu.edu>
2021-01-07 11:07:06 -0700

    Add searchbox component

---
+++ client/src/components/SearchBox.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const SearchBox = ({ history }) => {
+  const [name, setName] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    history.push(`/search/name/${name}`);
+    setName('');
+  };
+
+  return (
+    <form className='search' onSubmit={submitHandler}>
+      <div className='row'>
+        <input
+          type='text'
+          name='q'
+          id='q'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className='primary' type='submit'>
+          <i className='fa fa-search'></i>
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBox;
