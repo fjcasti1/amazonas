ba2639e57d5033578011193a8386c25a71fd4d96
Francisco Castillo <fjcasti1@asu.edu>
2021-01-07 23:28:21 -0700

    Separate utils file into utils dir. Create getSearchQuery util function

---
+++ client/src/utils/priceLevels.js
@@ -0,0 +1,27 @@
+export const prices = [
+  {
+    range: 'Any',
+    min: 'any',
+    max: 'any',
+  },
+  {
+    range: '$1 to $10',
+    min: 1,
+    max: 10,
+  },
+  {
+    range: '$10 to $100',
+    min: 10,
+    max: 100,
+  },
+  {
+    range: '$100 to $500',
+    min: 100,
+    max: 500,
+  },
+  {
+    range: '$500 to $1000',
+    min: 500,
+    max: 1000,
+  },
+];
