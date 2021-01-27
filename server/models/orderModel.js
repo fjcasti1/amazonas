e7e24a202417230220b863e43de1dd7dc306d299
Francisco Castillo <fjcasti1@asu.edu>
2021-01-27 16:54:18 -0700

    Change shipping address object in model to fit stripe api

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -28,8 +28,10 @@ const orderSchema = new mongoose.Schema(
     shippingAddress: {
       firstName: { type: String, required: true },
       lastName: { type: String, required: true },
-      address: { type: String, required: true },
+      line1: { type: String, required: true },
+      line2: { type: String },
       city: { type: String, required: true },
+      state: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
