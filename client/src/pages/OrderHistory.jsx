0a837094ca3571e960bc3894300a98adba7adb39
Francisco Castillo <fjcasti1@asu.edu>
2021-01-27 13:14:25 -0700

    Format dates with Moment

--- client/src/pages/OrderHistory.jsx
+++ client/src/pages/OrderHistory.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Spinner from '../components/Spinner';
 import Alert from '../components/Alert';
 import { getMyOrders } from '../actions/orderActions';
+import Moment from 'react-moment';
 
 const OrderHistory = ({ history }) => {
   const dispatch = useDispatch();
@@ -14,45 +15,57 @@ const OrderHistory = ({ history }) => {
 
   return (
     <div>
-      <h1>Order History</h1>
-      {loading ? (
-        <Spinner />
-      ) : error ? (
-        <Alert>{error}</Alert>
-      ) : (
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>DATE</th>
-              <th>TOTAL</th>
-              <th>PAID</th>
-              <th>DELIVERED</th>
-              <th>ACTIONS</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <tr key={order._id}>
-                <td>{order._id}</td>
-                <td>{order.createdAt}</td>
-                <td>${order.totalPrice}</td>
-                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
-                <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : 'No'}</td>
-                <td>
-                  <button
-                    type='button'
-                    className='small'
-                    onClick={() => history.push(`/orders/${order._id}`)}
-                  >
-                    Details
-                  </button>
-                </td>
+      <div className='container'>
+        <h1>Order History</h1>
+        {loading ? (
+          <Spinner />
+        ) : error ? (
+          <Alert>{error}</Alert>
+        ) : (
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>DELIVERED</th>
+                <th>ACTIONS</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>
+                    <Moment element='span' format='MMM DD, YYYY' date={order.createdAt} />
+                  </td>
+                  <td>$ {order.price.total}</td>
+                  <td>
+                    {order.isDelivered ? (
+                      <Moment
+                        element='span'
+                        format='MMM DD, YYYY'
+                        date={order.deliveredAt}
+                      />
+                    ) : (
+                      'No'
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      type='button'
+                      className='small'
+                      onClick={() => history.push(`/orders/${order._id}`)}
+                    >
+                      Details
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 };
