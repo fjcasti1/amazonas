03fe98b96508dc5fbce32f8470c56f247ef24551
Francisco Castillo <fjcasti1@asu.edu>
2021-01-27 13:19:38 -0700

    Change order model to have paymentMethod and price objects

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -26,28 +26,36 @@ const orderSchema = new mongoose.Schema(
       required: true,
     },
     shippingAddress: {
-      fullName: { type: String, required: true },
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
     },
-    paymentMethod: { type: String, required: true },
-    paymentResult: {
-      id: { type: String },
-      status: { type: String },
-      update_time: { type: String },
-      email_address: { type: String },
+    paymentMethod: {
+      option: { type: String }, // Card or Paypal
+      paypal_id: { type: String }, // Transaction Id
+      card_network: { type: String },
+      card_last4: { type: Number },
     },
-    itemsPrice: { type: Number, required: true },
-    shippingPrice: { type: Number, required: true },
-    taxPrice: { type: Number, required: true },
-    totalPrice: { type: Number, required: true },
+    // paymentResult: {
+    //   id: { type: String },
+    //   status: { type: String },
+    //   update_time: { type: String },
+    //   email_address: { type: String },
+    // },
+    price: {
+      items: { type: Number, required: true },
+      shipping: { type: Number, required: true },
+      tax: { type: Number, required: true },
+      total: { type: Number, required: true },
+    },
     isPaid: { type: Boolean, required: true, default: false },
     isDelivered: { type: Boolean, required: true, default: false },
     paidAt: { type: Date },
     deliveredAt: { type: Date },
-    seller: { type: mongoose.Schema.Types.ObjectId },
+    sellers: [{ type: mongoose.Schema.Types.ObjectId }],
   },
   {
     timestamps: true,
